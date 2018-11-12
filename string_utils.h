39bb0f966261436ca0c650b0e4eb31aa04293030
Gregory Smith <gregory.smith@autodesk.com>
2018-11-12 17:12:03 +0000

    Refactor for clarity and fix array_string for constexpr

--- string_utils.h
+++ string_utils.h
@@ -1,4 +1,6 @@
-namespace detail
+#pragma once
+
+namespace num_to_string_impl
 {
   template <unsigned... digits>
   struct to_chars {
@@ -19,7 +21,7 @@ namespace detail
 // Use this like:
 // num_to_string<42>::value;
 template <unsigned num>
-struct num_to_string : detail::explode<num> {};
+struct num_to_string : num_to_string_impl::explode<num> {};
 
 
 // Compile time constant string.
@@ -27,6 +29,7 @@ template <unsigned tSIZE>
 class array_string {
 public:
   constexpr array_string(const char (str)[tSIZE + 1])
+    : m_chars{{0}}
   {
     for (unsigned i = 0; i <= tSIZE; ++i) {
       m_chars[i] = str[i];
@@ -35,6 +38,7 @@ public:
 
   template <unsigned tSIZE_1>
   constexpr array_string(const array_string<tSIZE_1>& str1, const array_string<tSIZE - tSIZE_1>& str2)
+    : m_chars{{0}}
   {
     for (int i = 0; i < tSIZE_1; ++i) {
       m_chars[i] = str1[i];
@@ -65,7 +69,7 @@ private:
 // auto str = literal("Hello world");
 // Without having to include the string length argument.
 template <unsigned tSIZE_PLUS_1>
-constexpr array_string<tSIZE_PLUS_1 - 1> literal(const char (&lit)[tSIZE_PLUS_1])
+constexpr array_string<tSIZE_PLUS_1 - 1> make_string(const char (&lit)[tSIZE_PLUS_1])
 {
   return array_string<tSIZE_PLUS_1 - 1>(lit);
 }
@@ -79,4 +83,37 @@ constexpr array_string<tSIZE_1 + tSIZE_2> operator+(
 {
   return array_string<tSIZE_1 + tSIZE_2>(str1, str2);
 }
+
+// Length of string (not including terminating null).
+constexpr unsigned length(const char* str)
+{
+    int result = 0;
+    while (*str) {
+        ++result;
+        ++str;
+    }
+    return result;
+}
+
+
+// This implementation of concatenation works but with some fairly nasty restrictions.
+/* template <const char*, typename, const char*, typename> */
+/* struct concat_impl; */
+
+/* template <int... I> */
+/* using IntSeq = std::integer_sequence<int, I...>; */
+/* template <int N> */
+/* using MakeIntSeq = std::make_integer_sequence<int, N>; */
+
+/* template <const char* tSTR1, int... tINT_SEQ1, const char* tSTR2, int... tINT_SEQ2> */
+/* struct concat_impl<tSTR1, IntSeq<tINT_SEQ1...>, tSTR2, IntSeq<tINT_SEQ2...>> { */
+/*     static constexpr const char value[] { */
+/*         tSTR1[tINT_SEQ1]..., tSTR2[tINT_SEQ2]..., 0 */
+/*     }; */
+/* }; */
+
+/* template <const char* tSTR1, const char* tSTR2> */
+/* constexpr auto concat { */
+/*     concat_impl<tSTR1, MakeIntSeq<length(tSTR1)>, tSTR2, MakeIntSeq<length(tSTR2)>>::value */
+/* }; */
 
