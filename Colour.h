96053374f24a9dddccad4115e463d0fac6e4726d
Gregory Smith <gregory.smith@autodesk.com>
2018-11-12 17:14:51 +0000

    Add colours and struct to convert from foreground to background

---
+++ Colour.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "string_utils.h"
+
+// Struct for defining colours and converting them to their bash representation.
+template <unsigned tVALUE>
+struct colour {
+    constexpr static auto as_bash = make_string("\\033[") + make_string(num_to_string<tVALUE>::value) + make_string("m");
+};
+
+namespace to_background_impl {
+  // Convert a foreground colour to a background colour.
+  template <unsigned tFOREGROUND_VALUE>
+  colour<tFOREGROUND_VALUE + 10> background_equivalent(colour<tFOREGROUND_VALUE>);
+}
+
+template <typename tCOLOUR>
+struct to_background {
+  constexpr static auto as_bash = decltype(to_background_impl::background_equivalent(tCOLOUR()))::as_bash;
+};
+
+// Specific colour values.
+using BLACK = colour<30>;
+using RED = colour<31>;
+using GREEN = colour<32>;
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 /* #include <cstdio> */
 #include <iostream>
 #include "string_utils.h"
-
-int constexpr length(const char* str)
-{
-  return *str ? 1 + length(str + 1) : 0;
-}
+#include "Colour.h"
 
 
 int main()
@@ -13,10 +9,13 @@ int main()
   /* constexpr const char* prompt = "\\w"; */
   /* fwrite(prompt, length(prompt), 1, stdout); */
 
-  auto str1 = literal(num_to_string<25>::value);
-  auto str2 = literal("Hello ");
-  auto str3 = str2 + str1;
-  std::cout << str3.c_str() << std::endl;
+  /* auto str1 = literal(num_to_string<25>::value); */
+  /* auto str2 = literal("Hello "); */
+  /* auto str3 = str2 + str1; */
+  /* std::cout << str3.c_str() << std::endl; */
+
+  std::cout << RED::as_bash.c_str() << std::endl;
+  std::cout << to_background<RED>::as_bash.c_str() << std::endl;
 }
 
 
