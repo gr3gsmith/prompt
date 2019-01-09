b5ba6ef4421a67cfbc5aefade5d326de9e928e85
Gregory Smith <gregory.smith@autodesk.com>
2019-01-09 16:12:07 +0000

    Allow generation of cygwin or git bash specific prompt.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include "segment.h"
 
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "function smithg_powerline_prompt {" << std::endl;
+  std::cout << "function smithg_prompt {" << std::endl;
 
   std::cout << "PS1=\"\"" << std::endl;
 
-  auto prompt = segment_sequence(
+  auto basicPrompt = segment_sequence(
     make_segment<DARK_GREY, WHITE>(make_string(" \\w ")),
     segment_sequence(
       git_segment<WHITE, BLACK>(),
@@ -18,7 +18,26 @@ int main()
     )
   );
 
-  std::cout << prompt.as_bash().c_str() << std::endl;
+  if (argc == 2) {
+    std::string shellType = argv[1];
+    if (shellType == "cygwin") {
+      auto cygwinPrompt = segment_sequence(
+					   make_segment<MAGENTA, BLACK>(make_string(" Cygwin ")),
+					   basicPrompt
+					   );
+      std::cout << cygwinPrompt.as_bash().c_str() << std::endl;
+    } else if (shellType == "gitbash") {
+      auto gitBashPrompt = segment_sequence(
+					    make_segment<GREEN, BLACK>(make_string(" Git Bash ")),
+					    basicPrompt
+					    );
+      std::cout << gitBashPrompt.as_bash().c_str() << std::endl;
+    } else {
+      std::cout << basicPrompt.as_bash().c_str() << std::endl;
+    }
+  } else {
+    std::cout << basicPrompt.as_bash().c_str() << std::endl;
+  }
 
   std::cout << "}" << std::endl;
 }
